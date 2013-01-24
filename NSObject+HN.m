7b1171ab73bda306050091db6bc1bd8f1ebb9f2b
Josh Chung <minorblend@gmail.com>
2013-01-24 13:52:37 +0900

    changed implementation of performBlock:afterDelay: using dispatch_after

--- NSObject+HN.m
+++ NSObject+HN.m
@@ -29,13 +29,8 @@
 
 @implementation NSObject (HN)
 
-- (void)addBlockOperation:(NSBlockOperation *)operation {
-  [[NSOperationQueue mainQueue] addOperation:operation];
-}
-
 - (void)performBlock:(void (^)(void))block afterDelay:(NSTimeInterval)delay {
-  [self performSelector:@selector(addBlockOperation:)
-             withObject:[NSBlockOperation blockOperationWithBlock:block]
-             afterDelay:delay];
+  dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delay * NSEC_PER_SEC);
+  dispatch_after(popTime, dispatch_get_main_queue(), block);
 }
 @end
