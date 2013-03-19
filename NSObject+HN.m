8192b916c83fe36d66650cc3713cb11d58f9f08a
Josh Chung <minorblend@gmail.com>
2013-03-19 23:45:48 +0900

    added enumeratePropertiesUsingBlock: and setPropertiesFrom:

--- NSObject+HN.m
+++ NSObject+HN.m
@@ -27,10 +27,35 @@
 
 #import "NSObject+HN.h"
 
+#import <objc/runtime.h>
+
 @implementation NSObject (HN)
 
 - (void)performBlock:(void (^)(void))block afterDelay:(NSTimeInterval)delay {
   dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delay * NSEC_PER_SEC);
   dispatch_after(popTime, dispatch_get_main_queue(), block);
+}
+
+- (void)enumeratePropertiesUsingBlock:(void (^)(NSString *))block {
+  unsigned int outCount, i;
+  objc_property_t *properties = class_copyPropertyList([self class], &outCount);
+  for (i = 0; i < outCount; i++) {
+    objc_property_t property = properties[i];
+    const char *property_name = property_getName(property);
+    if (property_name) {
+      NSString *propertyName = [NSString stringWithUTF8String:property_name];
+      if (block)
+        block(propertyName);
+    }
+  }
+  
+  free(properties);
+}
+
+- (void)setPropertiesFrom:(id)source {
+  [self enumeratePropertiesUsingBlock:^(NSString *propertyName) {
+    if ([source respondsToSelector:NSSelectorFromString(propertyName)])
+      [self setValue:[source valueForKey:propertyName] forKey:propertyName];
+  }];
 }
 @end
